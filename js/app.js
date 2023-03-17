a391edbdca5a6cd592773cf6dbbb74ef37f17ab5
sigfried <sigfriedga@hotmail.com>
2023-03-17 13:54:41 +0100

    Creation list of incomes and expenses and dynamic functionality

--- js/app.js
+++ js/app.js
@@ -1,6 +1,6 @@
 const incomes = [
     new Income('Salario', 2100.45),
-    new Income('Venta Wii U', 250.50) 
+    new Income('Venta Wii U', 250.50)
 ];
 
 const bills = [
@@ -8,31 +8,33 @@ const bills = [
     new Bills('Luz', 98)
 ];
 
-let loadApp = () =>{
+let loadApp = () => {
     loadHero();
+    loadIncomes();
+    loadBills();
 }
 
-let totalIncomes = () =>{
+let totalIncomes = () => {
     let totalIncome = 0;
-    for(let income of incomes){
+    for (let income of incomes) {
         totalIncome += income.value;
     }
     return totalIncome;
 }
 
-let totalBills = () =>{
+let totalBills = () => {
     let totalBill = 0;
-    for(bill of bills){
+    for (bill of bills) {
         totalBill += bill.value
     }
     return totalBill;
-    
+
 }
 
-let loadHero = () =>{
+let loadHero = () => {
     let budget = totalIncomes() - totalBills();
     let percentBill = totalBills() / totalIncomes();
-    
+
     document.getElementById('budget').innerHTML = formatCurrency(budget);
     document.getElementById('percent').innerHTML = formatPercent(percentBill);
     document.getElementById('incomes').innerHTML = formatCurrency(totalIncomes());
@@ -40,9 +42,83 @@ let loadHero = () =>{
 }
 
 const formatCurrency = (value) => {
-    return value.toLocaleString('es-ES', {style: 'currency', currency: 'EUR', minimumFractionDigits: 2});
+    return value.toLocaleString('es-ES', {
+        style: 'currency',
+        currency: 'EUR',
+        minimumFractionDigits: 2
+    });
 }
 
 const formatPercent = (value) => {
-    return value.toLocaleString('es-ES', {style:'percent', minimumFractionDigits:2});
+    return value.toLocaleString('es-ES', {
+        style: 'percent',
+        minimumFractionDigits: 2
+    });
+}
+/* Load Incomes template */
+const loadIncomes = () => {
+    let incomesHTML = '';
+    for (let income of incomes) {
+        incomesHTML += createIncomeHTML(income);
+    }
+    document.getElementById('list-incomes').innerHTML = incomesHTML;
+}
+
+const createIncomeHTML = (income) => {
+    let incomeHTML = `
+              <div class="element clineUpStyles">
+            <div class="element_description">${income.description}</div>
+            <div class="right clineUpStyles">
+              <div class="element_value">+ ${formatCurrency(income.value)}</div>
+              <div class="element_delete">
+                <button class="element_delete--btn">
+                  <ion-icon name="close-circle-outline"
+                  onclick='deleteIncome(${income.id})'></ion-icon>
+                </button>
+              </div>
+            </div>
+          </div>
+    `;
+    return incomeHTML;
+}
+
+const deleteIncome = (id) => {
+    let deleteIndex = incomes.findIndex(income => income.id === id);
+    incomes.splice(deleteIndex, 1);
+    loadHero();
+    loadIncomes();
+}
+
+const loadBills = () => {
+    let billsHTML = '';
+    for (let bill of bills) {
+        billsHTML += createBilleHTML(bill);
+    }
+    document.getElementById('list-bills').innerHTML = billsHTML;
+}
+
+const createBilleHTML = (bill) => {
+    let billHTML = `
+              <div class="element clineUpStyles">
+            <div class="element_description">${bill.description}</div>
+            <div class="right clineUpStyles">
+              <div class="element_value">- ${formatCurrency(bill.value)}</div>
+              <div class="element_percent">${formatPercent(bill.value/totalBills())}</div>
+              <div class="element_delete">
+                <button class="element_delete--btn">
+                  <ion-icon name="close-circle-outline"
+                  onclick='deleteBill(${bill.id})'></ion-icon>
+                </button>
+              </div>
+            </div>
+          </div>
+    `;
+    return billHTML;
+}
+
+const deleteBill = (id) => {
+    let deleteIndex = bills.findIndex(bill => bill.id === id);
+    bills.splice(deleteIndex, 1);
+    loadHero();
+    loadBills();
 }
